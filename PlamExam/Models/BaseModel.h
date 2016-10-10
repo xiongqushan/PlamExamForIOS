f25a618cc52f3189ef225a7a4aa03577844ead43
Jeremy7937 <guokai@hzadmindeiMac.local>
2016-10-10 17:31:57 +0800

    add testFile

---
+++ PlamExam/Models/BaseModel.h
@@ -0,0 +1,13 @@
+//
+//  BaseModel.h
+//  PlamExam
+//
+//  Created by 郭凯 on 16/10/10.
+//  Copyright © 2016年 guokai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+@end
---
+++ PlamExam/Models/BaseModel.m
@@ -0,0 +1,13 @@
+//
+//  BaseModel.m
+//  PlamExam
+//
+//  Created by 郭凯 on 16/10/10.
+//  Copyright © 2016年 guokai. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@implementation BaseModel
+
+@end
