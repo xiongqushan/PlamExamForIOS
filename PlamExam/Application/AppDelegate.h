885591e07f7ac79fe7207472e6450927dad32cef
Jeremy7937 <guokai@hzadmindeiMac.local>
2016-10-10 15:16:23 +0800

    add project

---
+++ PlamExam/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlamExam
+//
+//  Created by 郭凯 on 16/10/9.
+//  Copyright © 2016年 guokai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlamExam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlamExam
+//
+//  Created by 郭凯 on 16/10/9.
+//  Copyright © 2016年 guokai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
