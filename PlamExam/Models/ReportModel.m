1c88c214266dbc591fdfaeb114b1039a9456acc9
xiongqushan <45429827@qq.com>
2016-10-20 19:33:10 +0800

    temp submit

--- PlamExam/Models/ReportModel.m
+++ PlamExam/Models/ReportModel.m
@@ -41,6 +41,9 @@
         HttpRequestResult<ReportBatch *> *result = httpRequestResult;
         if(httpRequestResult.IsHttpSuccess && httpRequestResult.HttpResult.Code>0){
             result.Data = [ReportBatch yy_modelWithJSON:result.HttpResult.Result];
+            NSString* aa=result.Data.CheckUnitCode;
+            ReportSimple *rrr= result.Data.Reports[0];
+            NSString* aaaaaa= rrr.CheckUnitCode;
         }
         callBack(result);
         
